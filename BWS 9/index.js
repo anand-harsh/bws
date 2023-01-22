e338920d4a39d834e5ff1dca121b1956a99eaeaa
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2023-01-22 23:04:44 +0530

    Update (BWS #9)

--- BWS 9/index.js
+++ BWS 9/index.js
@@ -1,12 +1,14 @@
 // Step 1: Fill in these three consts by selecting the right nodes by their HTML ID's
-const name = document.getElementById(""); // Fill this in 
-const age = document.getElementById(""); // Fill this in.
-const dogYears = document.getElementById(""); // Fill this in. 
+const name = document.getElementById("name"); // Fill this in 
+const age = document.getElementById("age"); // Fill this in.
+const dogYears = document.getElementById("dogyears"); // Fill this in. 
 
 // Step 2. Set the values.
 // Example: myNode.innerText = "something"
 // Set `name` and `age` in here
 
+name.innerText="Sam";
+age.innerText=10;
 
 function showDogYears(age) {
   // Step 3. Cast `age` as a number
@@ -15,7 +17,7 @@ function showDogYears(age) {
 }
 
 // Step 4. Set the parameter of showDogyears()
-dogYears.innerText = showDogYears()
+dogYears.innerText = showDogYears(5)
 
 // Step 5. Select all the .fake-input classes and give them the class of .real-input. This may take a little "googling" to find the solution, but that's half of what web development is all about. 
 const fakeInputs = document.querySelectorAll(".fake-input")
