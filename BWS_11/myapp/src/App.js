9c0e2b3dbd8cf4f16973973937f6268acbb68d8e
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2023-02-05 10:15:23 +0530

    Update

--- BWS_11/myapp/src/App.js
+++ BWS_11/myapp/src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react'
-import Item from './MyItem'
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import Item from "./MyItem";
 
+class StarWars extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      name: null,
+      height: null,
+      homeworld: null,
+      films: []
+    }
+  }
+
+  getNewCharacter() {
+    console.log("get noew character");
+    this.setState({
+      name: 'Luke',
+      height: 172,
+      homeworld: 'Tatooine',
+      films: ['Item1', 'Item2']
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <p>{this.state.height}</p>
+        <p>Homeworld: {this.state.homeworld}</p>
+        <ul>
+          <li>{this.state.films}</li>
+        </ul>
+        <button
+          type="button"
+          onClick={() => this.getNewCharacter()}
+          className="btn"
+        >
+          Click Me
+        </button>
+      </div>
+    );
+  }
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Item name="Harsh" age="19"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StarWars />
       </header>
     </div>
   );
